8870c0a980a2bda5516c58bd254fc09abd969247
Tyson <bangphuongqb146@gmail.com>
2026-05-11 19:13:31 +0700

    Continue pushing code

---
+++ vid20.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm TaFi");
+
+const r = 3.14;
+let a = 1, b = 3;
+console.log(a, " + ", b, " = ", a+b);
+console.log(a, " / ", b, " = ", a/b);
+console.log("r = ", r);
+a++;
+console.log(a + " % " + b + " = " + a%b);
+console.log('5' == 5);
+console.log('5' === 5);
+// console.log('5' == 5);
+// console.log('5' === 5);
+/* 
+console.log('5' == 5);
+console.log('5' === 5);
+*/
+
+// console.error("This is an error message");
+// console.warn("This is a warning message");
+
+console.log("%cWarning!", "color: red; font-size: 20px; font-weight: bold;");
